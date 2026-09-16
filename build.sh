dcaaefc4b70d980f475780f67a9fc8e871e627ae
Melih Pala <melihkayhanpala@gmail.com>
2026-09-16 13:27:04 +0300

    build.sh scripti duzeltildi

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+#!/usr/bin/env bash
+set -o errexit
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("articles.urls")),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
